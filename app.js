7633b9ed91aaa13dc956ddeda55cc5b6ea766cc8
tetiana22 <134863156+tetiana22@users.noreply.github.com>
2024-08-04 15:17:23 +0100

    add localhost in app

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.use(bodyParser.json());
 app.use(
   cors({
     origin: "http://localhost:3000",
+    origin: "http://localhost:5175",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
