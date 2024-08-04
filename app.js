e1ebd331761a493e49f1af9bd620c863b1653582
tetiana22 <134863156+tetiana22@users.noreply.github.com>
2024-08-04 20:01:54 +0100

    add filename, dirname

--- app.js
+++ app.js
@@ -2,8 +2,17 @@ import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
 import cors from "cors";
-
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/donation.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "src/partials/donation.html"));
+});
 app.use(bodyParser.json());
 app.use(
   cors({
