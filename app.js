3efbeff470486e8ecc24678a75be51585f1b88aa
tetiana22 <134863156+tetiana22@users.noreply.github.com>
2024-08-14 13:10:43 +0100

    add path

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
 import cors from "cors";
-
+import path from "path"; // Імпортуємо path
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
+
+app.use(express.static(path.join(__dirname, "dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 app.use(bodyParser.json());
 app.use(
