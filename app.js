7e487d9b9a8f17442f33b86a06458940f540485d
tetiana22 <134863156+tetiana22@users.noreply.github.com>
2024-08-14 14:05:54 +0100

    chng on dist

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
 
-app.use(express.static(path.join(__dirname, "src")));
+app.use(express.static(path.join(__dirname, "dist")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "src", "index.html"));
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 app.use(bodyParser.json());
