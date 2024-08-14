0abe8441c0ba0aeab178cb1403233dec0d0354aa
tetiana22 <134863156+tetiana22@users.noreply.github.com>
2024-08-14 16:22:43 +0100

    vite

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "src")));
+app.use(express.static(path.join(__dirname, "dist")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "src", "index.html"));
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 app.use(bodyParser.json());
